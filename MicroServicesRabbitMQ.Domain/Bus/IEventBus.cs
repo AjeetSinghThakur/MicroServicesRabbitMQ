d4d9c9833b0c3c6de6fc5455302d5abad9118100
AjeetSingh <thakurajeet99@gmail.com>
2021-11-24 23:29:41 +0530

    RabbitMQBus added.

--- MicroServicesRabbitMQ.Domain/Bus/IEventBus.cs
+++ MicroServicesRabbitMQ.Domain/Bus/IEventBus.cs
@@ -8,7 +8,7 @@ namespace MicroServicesRabbitMQ.Domain.Bus
     {
         Task SendCommand<T>(T command) where T : Command;
 
-        void Publish<T>(T command) where T : Event;
+        void Publish<T>(T @event) where T : Event;
 
         void Subscribe<T, TH>()
             where T : Event
