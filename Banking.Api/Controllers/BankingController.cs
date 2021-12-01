ba3b662be4ccb11ae42b6a114ce9c09788299418
AjeetSingh <thakurajeet99@gmail.com>
2021-12-01 07:56:22 +0530

    Modified code to get the data from DB

---
+++ Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,26 @@
+using Banking.Application.Interfaces;
+using Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly ILogger<BankingController> _logger;
+        private readonly IAccountService _accountService;
+
+        public BankingController(ILogger<BankingController> logger, IAccountService accountService)
+        {
+            _logger = logger;
+            _accountService = accountService;
+        }
+
+        [HttpGet(Name = "GetAccounts")]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
